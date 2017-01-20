d54d608e1b958ab9cec71f8dc612eb8a758174f7
drwatley <atoussi@my.bcit.ca>
2017-01-20 13:56:58 -0800

    this is a message

---
+++ Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string path = MapPath("~/uploads"); 
+        string[] files = Directory.GetFiles(path);
+        DropDownList1.DataSource = files;
+        DropDownList1.DataBind();
+    }
+
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        
+    }
+}
